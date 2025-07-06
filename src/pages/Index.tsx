fe2a10fc94292faf5b76660f853b7e0f8bc3bbc2
SKSritharan <sk.sritharan75@gmail.com>
2025-07-06 13:13:58 +0530

    Added SEO Contents

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -23,12 +23,12 @@ const Index = () => {
                 evidence-based, patient-centred care.
               </p>
               <div className="flex flex-col sm:flex-row gap-4 justify-centre lg:justify-start">
-                <NavLink
-                  to="/contact"
+                <a
+                  href="https://share.google/SUTyWWduaIEwU25z7"
                   className="bg-white text-blue-800 px-8 py-4 rounded-lg font-semibold hover:bg-gray-100 transition-colours"
                 >
                   Book an Appointment
-                </NavLink>
+                </a>
                 <NavLink
                   to="/services"
                   className="border-2 border-white text-white px-8 py-4 rounded-lg font-semibold hover:bg-white hover:text-blue-800 transition-colours"
